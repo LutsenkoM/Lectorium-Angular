e377dd7b446d7c55f30f96ef3bcc41fe27f72622
Max Lutsenko <lutsenko.maxim@gmail.com>
2019-04-11 00:46:18 +0300

    Create custom validator Reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,13 +15,15 @@ export class AppComponent implements OnInit{
     text: 'Нет'
   }];
 
+  charsCount = 5;
+
   form: FormGroup;
 
   ngOnInit() {
     this.form = new FormGroup({
       user: new FormGroup({
         email: new FormControl('', [Validators.required, Validators.email]),
-        pass: new FormControl('', Validators.required)
+        pass: new FormControl('', [Validators.required, this.checkForLength.bind(this)])
       }),
       country: new FormControl('ua'),
       answer: new FormControl('no ')
@@ -31,5 +33,15 @@ export class AppComponent implements OnInit{
 
   onSubmit() {
     console.log(this.form);
+  }
+
+  checkForLength(control: FormControl) {
+    if(control.value.length <= this.charsCount) {
+      return {
+        'lengthError' : true
+      }
+    }
+
+    return null;
   }
 }
