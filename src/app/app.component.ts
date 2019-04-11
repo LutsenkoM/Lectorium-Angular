afa51bf1ed360dcd842ff68bfedcc7155c7469a1
Maksym Lutsenko <maksym.m.lutsenko@gmail.com>
2019-04-11 19:04:27 +0300

    Setup server ( https://github.com/typicode/json-server )

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'angular';
+export class AppComponent{
+  cars = [
+    {
+      name: 'Ford',
+      color: 'white',
+      id: 1
+    }
+  ];
 }
