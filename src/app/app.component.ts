99b59ddd2385072d9855d32390240533a62b738d
Max Lutsenko <lutsenko.maxim@gmail.com>
2019-04-12 00:18:02 +0300

    Output data in template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,41 @@
 import {Component, OnInit} from '@angular/core';
 import {CarsService} from "./cars.service";
 
+interface Cars  {
+    name: string;
+    color: string;
+    id: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
-  cars = [];
+    cars: Cars[] = [];
+    carName: string = '';
 
-  constructor(private carsService: CarsService) {}
+    constructor(private carsService: CarsService) {}
 
-  ngOnInit() {
-  //   this.carsService
-  //       .getCars()
-  //       .subscribe((response) => {     get data on init
-  //         console.log(response);
-  //       });
-  }
+    ngOnInit() {
+    //   this.carsService
+    //       .getCars()
+    //       .subscribe((response) => {     get data on init
+    //         console.log(response);
+    //       });
+    }
 
     loadCars() {
       this.carsService
           .getCars()
-          .subscribe((response) => {
-            console.log(response);
+          .subscribe(response => {
+            // console.log(response);
+            this.cars = response;
           });
+    }
+
+    addCar() {
+
     }
 
 }
