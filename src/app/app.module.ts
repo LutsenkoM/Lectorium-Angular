379d3b63a207987e86aa7b91046367edea22736d
Maksym Lutsenko <maksym.m.lutsenko@gmail.com>
2019-04-19 13:07:09 +0300

    add lesson code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { CarsPageComponent } from './cars-page/cars-page.component';
+import { CarsService } from './cars.service';
+import { AppRoutingModule } from './app-routing.module';
+import { ColorDirective } from './shared/color.directive';
+import { HeaderComponent } from './header/header.component';
+import { CarPageComponent } from './cars-page/car-page/car-page.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    CarsPageComponent,
+    CarPageComponent,
+    ColorDirective,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [CarsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
