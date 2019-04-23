7c91e58bae0789c72944e6dbeb4726d2f1afab9e
Maksym Lutsenko <maksym.m.lutsenko@gmail.com>
2019-04-23 22:45:57 +0300

    Create animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+      trigger('clickedDiv', [
+          state('start', style({
+              backgroundColor: 'blue',
+              width: '200px',
+              height: '150px'
+          })),
+          state('end', style({
+              backgroundColor: 'red',
+              width: '400px',
+              height: '400px'
+          })),
+          transition('start  => end', animate(1500)),
+          transition('end  => start', animate('800ms 0.5s ease-out'))
+      ])
+  ]
 })
 export class AppComponent {
-  title = 'angular';
+  clickedDivState = 'start';
+
+  changeDivState() {
+      this.clickedDivState = 'end';
+      setTimeout(() => {
+          this.clickedDivState = 'start';
+      }, 3000);
+  }
 }
